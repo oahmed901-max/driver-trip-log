3ba3c620c50bc8af07fa1108f4173d81f5397a5b
oahmed901-max <oahmed901@gmail.com>
2026-09-10 16:20:56 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from driver_view import render_driver_view
+from supervisor_view import render_supervisor_view
+
+st.set_page_config(page_title="سجل حركة السواقين", page_icon="🚚", layout="centered")
+
+params = st.query_params
+token = params.get("d")
+
+if token:
+    render_driver_view(token)
+else:
+    render_supervisor_view()
